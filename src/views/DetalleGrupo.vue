911a12ca41c17628f120622f3ff356ab80d83617
Ildefonso <ponchovalo@outlook.com>
2020-09-03 15:41:23 -0500

    alumnos

--- src/views/DetalleGrupo.vue
+++ src/views/DetalleGrupo.vue
@@ -80,6 +80,7 @@
         </div>  
         <div class="text-center">
             <v-alert
+                v-if="(alumnos.length === 0)"
                 text
                 dense
                 color="teal"
@@ -92,10 +93,66 @@
                 class="ma-2"
                 color="primary"
                 outlined
-                @click="showgroupdialog">
+                @click="showalumndialog">
                 <v-icon left>face</v-icon>
                 Agregar nuevo Alumno
             </v-chip>
+
+            <v-row>
+                <v-dialog v-model="alumndialog" max-width="400px">
+                <v-card>
+                    <v-card-title>
+                    <span class="headline">Agregar Nuevo Alumno</span>
+                    </v-card-title>
+                    <v-card-text>
+                    <v-container> 
+                        <v-row>
+                        <v-col cols="12" sm="12" md="12">
+                            <v-text-field v-model="alumno.apellido.paterno"
+                            label="Apellido Paterno*" 
+                            required>
+                            </v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="12" md="12">
+                            <v-text-field v-model="alumno.apellido.materno"
+                            label="Apellido Materno*" 
+                            required>
+                            </v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="12" md="12">
+                            <v-text-field v-model="alumno.nombre"
+                            label="Nombre" 
+                            required>
+                            </v-text-field>
+                        </v-col>
+                        </v-row>
+                    </v-container>
+                    <small>*indica que el campo es requierido</small>
+                    </v-card-text>
+                    <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="red darken-1" text @click="closealumndialog">Close</v-btn>
+                    <v-btn color="teal darken-1" text @click="agregarAlumno(alumno)">Save</v-btn>
+                    </v-card-actions>
+                </v-card>
+                </v-dialog>
+            </v-row>
+        </div>
+        <div>
+            <v-data-table dense
+                v-if="!(alumnos.length === 0)"
+                :headers="headers"
+                :items="alumnos"
+                sort-by="apellido.paterno"
+                class="elevation-1">
+                <template v-slot:top>
+                    <v-toolbar flat color="white">
+                        <v-toolbar-title>Alumnos de : {{grupo.nombre}}</v-toolbar-title>
+                    </v-toolbar>
+                </template>
+                
+            </v-data-table>
+            
         </div>
     </v-container>
 </template>
@@ -106,16 +163,26 @@ export default {
   components: {
   },
   data(){
-      return{
-          grupoId: this.$route.params.id,
-          grupo:{}
-      }
+    return{
+        grupoId: this.$route.params.id,
+        grupo:{},
+        alumnos:[],
+        alumndialog: false,
+        alumno:{apellido:{}},
+        headers:[
+            {text:'Apellido Paterno',  value:'apellido.paterno'},
+            {text:'Apellido Materno', value:'apellido.materno'},
+            {text:'Nombre', value:'nombre'},
+            
+        ]
+    }
   },
   created(){
-      this.obtenerDatos()
+    this.obtenerDatos()
+    this.obtenerAlumnos()
   },
   methods:{
-      obtenerDatos(){
+    obtenerDatos(){
         this.axios.get('grupos/'+this.grupoId)
         .then((response) => {
             console.log(response.data)
@@ -124,7 +191,43 @@ export default {
         .catch((e)=>{
             console.log('error' + e);
         })
-      }
+    },
+    obtenerAlumnos(){
+        this.axios.get('alumnos/'+this.grupoId)
+        .then((response) => {
+            console.log(response.data)
+            this.alumnos = response.data;
+        })
+        .catch((e)=>{
+            console.log('error' + e);
+        })
+    },
+    showalumndialog(){
+        this.alumndialog = true
+    },
+    closealumndialog(){
+        this.alumndialog = false
+        this.limpiaralumndialog()
+    },
+    limpiaralumndialog(){
+        this.alumno.nombre='',
+        this.alumno.apellido={}
+    },
+    agregarAlumno(item){
+        item.grupo = this.grupo._id
+        this.axios.post('alumnos/new', item)
+        .then(res => {
+            this.closealumndialog();
+            this.obtenerAlumnos();
+
+            //mostrar alerta
+            //this.alerta.texto="Grupo Creado Correctamente"
+            //this.alerta.estado = true
+
+        }).catch( e => {
+            console.log(e)
+        })
+    }
   }
 
 }
